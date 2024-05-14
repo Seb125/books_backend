fb75a0594b790b7e77d63ce206401a0d561f6ea9
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-05-14 13:36:31 +0200

    done

--- main.py
+++ main.py
@@ -1,55 +1,54 @@
 from fastapi import FastAPI
-import random
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
-from pydantic import BaseModel
-
-
-
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
 
+# Allow requests from http://localhost:5173
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
-@app.get('/')
-async def root():
-    return {'example': 'This is an example 2'}
-
-@app.get('/random')
-async def get_random():
-    rn:int = random.randint(0, 100)
-    return {'number': rn}
-                            
-@app.get('/random/{limit}')
-async def get_random(limit: int):
-    rn:int = random.randint(0, limit)
-    return {'number': rn}
-
-
+# Pydantic model for book data, Pydantic is a data validation and settings management library
 class Book(BaseModel):
     name: str
     description: str
     created_at: datetime
     created_by: str
+    id: int = Field(default=None, description="Optional ID")
+
 
 # Beispielbuchdaten
 books_data = [
-    Book(name="Book 1", description="Description of Book 1", created_at=datetime.now(), created_by="User 1"),
-    Book(name="Book 2", description="Description of Book 2", created_at=datetime.now(), created_by="User 2"),
-    Book(name="Book 3", description="Description of Book 3", created_at=datetime.now(), created_by="User 3")
+    Book(name="Book 1", description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", created_at=datetime.now().replace(microsecond=0), created_by="User 1", id=0),
+    Book(name="Book 2", description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", created_at=datetime.now().replace(microsecond=0), created_by="User 2", id=1),
+    Book(name="Book 3", description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", created_at=datetime.now().replace(microsecond=0), created_by="User 3", id=2),
+    Book(name="Book 4", description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", created_at=datetime.now().replace(microsecond=0), created_by="User 1", id=3),
+    Book(name="Book 5", description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", created_at=datetime.now().replace(microsecond=0), created_by="User 2", id=4),
+    Book(name="Book 6", description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", created_at=datetime.now().replace(microsecond=0), created_by="User 3", id=5)
 ]
+
 
 @app.get('/books')
 async def get_books():
     return books_data
 
-@app.get('/books/{book_id}')
-async def get_book(book_id: int):
-    if book_id < len(books_data):
-        return books_data[book_id]
-    else:
-        return {"message": "Book not found"}
 
-@app.post('/books')
-async def create_book(book: Book):
-    books_data.append(book)
-    return {"message": "Book created successfully"}
+@app.put('/books')
+async def update_book(updated_book: Book):
+    try:
+        # Update the book in the books_data list
+        for index, book in enumerate(books_data):
+            if book.id == updated_book.id:
+                # Update the book at the found index
+                books_data[index] = updated_book
+        return {"message": "Book with ID {book_id} updated successfully"}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
